b008039431e6324262e8677fb4f1530a53a43fa0
tnarik <tnarik@gmail.com>
2015-01-21 10:03:48 +0000

    Generic resources updated.

--- README.md
+++ README.md
@@ -2,3 +2,14 @@
 
 TODO: Enter the cookbook description here.
 
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: Tnarik Innael (tnarik@lecafeautomatique.co.uk)
--- metadata.rb
+++ metadata.rb
@@ -1,8 +1,8 @@
 name             'asset_provider'
-maintainer       'The Authors'
-maintainer_email 'you@example.com'
+maintainer       'Tnarik Innael'
+maintainer_email 'tnarik@lecafeautomatique.co.uk'
 license          'all_rights'
 description      'Installs/Configures asset_provider'
-long_description 'Installs/Configures asset_provider'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
 version          '0.1.0'
 
