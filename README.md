a0ae92d703661d8a90489229344dd5af518e0f5b
tnarik <tnarik@gmail.com>
2015-01-21 09:58:45 +0000

    Initial commit

---
+++ README.md
@@ -0,0 +1,4 @@
+# asset_provider
+
+TODO: Enter the cookbook description here.
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'asset_provider'
+maintainer       'The Authors'
+maintainer_email 'you@example.com'
+license          'all_rights'
+description      'Installs/Configures asset_provider'
+long_description 'Installs/Configures asset_provider'
+version          '0.1.0'
+
